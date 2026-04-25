54ebb47535617f8e59bbaa19d71ef7b07bf52265
Samnang <youremail@gmail.com>
2026-04-25 21:51:35 +0700

    connet to database mongo

--- api/index.js
+++ api/index.js
@@ -1,5 +1,17 @@
+import dotenv from "dotenv";
+dotenv.config();
 import express from "express";
+import mongoose from "mongoose";
+import { error } from "node:console";
 
+mongoose
+  .connect(process.env.MONGO)
+  .then(() => {
+    console.log("mongo connecting");
+  })
+  .catch((error) => {
+    console.log(error);
+  });
 const app = express();
 
 app.listen(3000, () => {
