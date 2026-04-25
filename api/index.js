633b8f0267ab1a9fcdafb1a95183b43b8dc01a10
Samnang <youremail@gmail.com>
2026-04-25 22:20:18 +0700

    completed route api auth

--- api/index.js
+++ api/index.js
@@ -2,7 +2,8 @@ import dotenv from "dotenv";
 dotenv.config();
 import express from "express";
 import mongoose from "mongoose";
-import { error } from "node:console";
+import userRoutes from "./routes/user.route.js";
+import authRoutes from "./routes/auth.route.js";
 
 mongoose
   .connect(process.env.MONGO)
@@ -17,3 +18,7 @@ const app = express();
 app.listen(3000, () => {
   console.log("server listening on port 3000");
 });
+
+// route api
+app.use("/api/user", userRoutes);
+app.use("/api/auth", authRoutes);
